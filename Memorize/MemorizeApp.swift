774f31053a48b31df506289f3cc7625f1cefaf55
gauravpandey <gaurav91pandey@gmail.com>
2021-07-26 21:56:35 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Gaurav Pandey on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
