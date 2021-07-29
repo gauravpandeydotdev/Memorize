27b64fa1d70ad9f66481ab58312d107c90e4c796
gauravpandey <gaurav91pandey@gmail.com>
2021-07-28 22:29:13 -0700

    added HStack 1) encapsulated ZStack in a cardView 2) removed padding from ZStack

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            cardView()
+            cardView()
+            cardView()
+            cardView()
+        }
+        .padding()
+    }
+}
+
+struct cardView: View {
+    var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
                 .stroke(lineWidth: 3)
             
             Text("Hello, world!")
-        }.padding()
+        }
     }
 }
 
